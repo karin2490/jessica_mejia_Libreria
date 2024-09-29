fd97570a8c03dff613f42d4a6b592dce8cbdebaa
Jessica Mejia <jessica.mejia@ramva.com>
2024-09-28 23:16:17 -0600

    update analysis

--- vars/staticAnalysis.groovy
+++ vars/staticAnalysis.groovy
@@ -1,7 +1,6 @@
 def call(Map params = [:]) {
-    boolean shouldAbort = params.get('shouldAbort', false)
-    boolean performQualityCheck = params.get('performQualityCheck', false)
-    def currentBranch = env.BRANCH_NAME
+     boolean abortPipeline = params.get('abortPipeline', false)
+    boolean qualityGateCheck = params.get('qualityGateCheck', false)
 
     withSonarQubeEnv('SonarQube Local') {
         sh 'echo "Iniciando análisis de calidad de código..."'
@@ -9,19 +8,14 @@ def call(Map params = [:]) {
         timeout(time: 5, unit: 'MINUTES') {
             echo 'Esperando resultados del análisis...'           
 
-            if (performQualityCheck) {
+            if (qualityGateCheck) {
                  // Simulación de una llamada a un análisis de calidad
                 def qualityResult = 'PASSED' 
                 echo "Estado del Quality Gate: ${qualityResult}"
                
-                if (shouldAbort) {
-                        error 'El análisis ha fallado; el pipeline se aborta.'
-                } else if (currentBranch == 'main' || currentBranch.startsWith('bugfix')) {
-                        error "El Quality Gate ha fallado en la rama '${currentBranch}', abortando el pipeline."
-                    } else {
-                        echo "El Quality Gate ha fallado, pero se continúa en la rama '${currentBranch}'."
-                    }
-               
+               if(qualityResult == 'FAILED' && abortPipeline){
+                error 'El análisis ha fallado y se ha abortado el pipeline.'
+               }
             }
         }
     }
