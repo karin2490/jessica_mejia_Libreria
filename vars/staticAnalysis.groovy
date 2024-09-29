6df44c72dcb1c2360705f8059533984b8dcbd7bc
Jessica Mejia <jessica.mejia@ramva.com>
2024-09-28 23:08:54 -0600

    update analysis

--- vars/staticAnalysis.groovy
+++ vars/staticAnalysis.groovy
@@ -7,26 +7,25 @@ def call(Map params = [:]) {
         sh 'echo "Iniciando análisis de calidad de código..."'
         
         timeout(time: 5, unit: 'MINUTES') {
-            echo 'Esperando resultados del análisis...'
-
-            // Simulación de una llamada a un análisis de calidad
-            def qualityResult = 'PASSED' 
+            echo 'Esperando resultados del análisis...'           
 
             if (performQualityCheck) {
+                 // Simulación de una llamada a un análisis de calidad
+                def qualityResult = 'PASSED' 
                 echo "Estado del Quality Gate: ${qualityResult}"
-
-                if (qualityResult == 'FAILED') {
-                    if (shouldAbort) {
-                        error 'El Quality Gate ha fallado; el pipeline se aborta.'
-                    } else if (currentBranch == 'main' || currentBranch.startsWith('bugfix')) {
+               
+                if (shouldAbort) {
+                        error 'El análisis ha fallado; el pipeline se aborta.'
+                } else if (currentBranch == 'main' || currentBranch.startsWith('bugfix')) {
                         error "El Quality Gate ha fallado en la rama '${currentBranch}', abortando el pipeline."
                     } else {
                         echo "El Quality Gate ha fallado, pero se continúa en la rama '${currentBranch}'."
                     }
-                } else {
-                    echo "El Quality Gate ha sido aprobado, continuando el pipeline."
-                }
+               
             }
         }
     }
 }
+
+
+
